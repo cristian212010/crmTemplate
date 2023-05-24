a373e9fd671d63a1f30ae186795c6b15681aa316
cristian212010 <cristiancamilo212010@gmail.com>
2023-05-24 08:50:36 -0500

    feat: :sparkles: Modifique archivo db.sql
    se le agregaron las filas que faltaban

--- Estudiantes/db.sql
+++ Estudiantes/db.sql
@@ -2,6 +2,15 @@ CREATE DATABASE campus;
 
 USE campus;
 
-CREATE TABLE campers(
-
-);
+CREATE TABLE
+    campers(
+        id INT primary key AUTO_INCREMENT,
+        NOMBRES VARCHAR (50) NOT NULL,
+        direccion VARCHAR (50) NOT NULL,
+        logros VARCHAR (60),
+        especialida VARCHAR (60) NOT NULL,
+        skills FLOAT (60) NOT NULL,
+        ingles VARCHAR (60) NOT NULL,
+        ser FLOAT (60) NOT NULL,
+        review VARCHAR (60) NOT NULL,
+    );
